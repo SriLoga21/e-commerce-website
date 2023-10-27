670377e184ca108f58fd61ab9c182e48b01dde6b
Sri Loga G <136245272+SriLoga21@users.noreply.github.com>
2023-10-27 21:45:32 +0530

    Add files via upload

---
+++ src/component/footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AboutPage from "../pages/AboutPage";
+import ProductDetails from "../pages/ProductDetails";
+
+const Footer = () => {
+  return (
+    <>
+        <AboutPage></AboutPage>
+        <ProductDetails></ProductDetails>
+    </>
+  );
+};
+
+export default Footer;
---
+++ src/component/navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AboutPage from "../pages/AboutPage";
+import ProductDetails from "../pages/ProductDetails";
+
+const Navbar = () => {
+  return (
+    <>
+        <AboutPage></AboutPage>
+        <ProductDetails></ProductDetails>
+    </>
+  );
+};
+
+export default Navbar;
